b9df21a189a20654d03f3bad69475c30036420c9
RachelNS <rachelnelsonschille@gmail.com>
2020-12-16 21:01:25 -0800

    added moment script, displayed current date at top of page

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("fuck")
+var userInput = document.querySelector(".form-control")
+
+//Current date will be displayed at the top 
+$("#currentDay").text(moment().toDate());
+
+
+//When user clicks the "save" button, their input will be saved in local storage
+var buttonTest = $(".saveBtn").click(function() {
+    console.log(userInput.value);
+    localStorage.setItem("activities", userInput.value);
+})
+
+

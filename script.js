5266f779e75bfdef6ddeb9135519855867425527
RachelNS <rachelnelsonschille@gmail.com>
2020-12-16 23:18:11 -0800

    worked out Moment, displayed time and date correctly, can now access current time as shown in console log prompts

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 var userInput = document.querySelector(".form-control")
+var hour = $("#experiment");
+var now = moment();
 
 //Current date will be displayed at the top 
-$("#currentDay").text(moment().toDate());
+$("#currentDay").text(now.format("dddd, MMMM Do YYYY"));
 
 
 //When user clicks the "save" button, their input will be saved in local storage
 var buttonTest = $(".saveBtn").click(function() {
     console.log(userInput.value);
     localStorage.setItem("activities", userInput.value);
+    $(".form-control").value(localStorage.getItem("activities"));
 })
 
 
+console.log(moment.parseZone());
+
+if(now.isAfter("2021-11-18", "hour")) {
+    $(".hour").addClass("future")
+}
+
+else{
+    $(".hour").addClass("past")
+}
+
+console.log(now.hour());
+
+console.log(hour.text);

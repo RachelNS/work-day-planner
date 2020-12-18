8de8cc7782faabd0ea9d2bf18899ac7fc58b1d06
RachelNS <rachelnelsonschille@gmail.com>
2020-12-17 22:37:46 -0800

    final commit

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
+// Grabs the moment object
 var now = moment();
+
+// Grabs the current hour from the moment object
 var currentHour = now.format("HH");
+
+// Grabs an array of the hour blocks 
 var hourValues = $(".hour")
 
-console.log(hourValues)
+//Current date will be displayed at the top of the page
+$("#currentDay").text(now.format("dddd, MMMM Do YYYY"));
 
-// hourValues.each( function(index) {
-//     console.log($(hourValues[index]).attr("data-time"));
-// });
+//For each hour block, check its time data attribute against the current hour
+for (i = 0; i < hourValues.length; i++) {
 
-for(i=0; i < hourValues.length; i++){
-    // console.log($(hourValues[i]).attr("data-time"));
-
-    if(currentHour < $(hourValues[i]).attr("data-time")){
+    // If the current hour is less than the time data attribute, the block gets the "future" class
+    if (currentHour < $(hourValues[i]).attr("data-time")) {
         $(hourValues[i]).next().addClass("future");
     }
-    else if(currentHour===$(hourValues[i]).attr("data-time")){
+    // If the current hour is equal to the time data attribute, the block gets the "present" class
+    else if (currentHour === $(hourValues[i]).attr("data-time")) {
         $(hourValues[i]).next().addClass("present");
     }
-    else{
+    // Otherwise, the block gets the "past" class
+    else {
         $(hourValues[i]).next().addClass("past");
     }
 }
 
-
-//Current date will be displayed at the top 
-$("#currentDay").text(now.format("dddd, MMMM Do YYYY"));
-
-
 //When user clicks the "save" button, their input will be saved in local storage
-$(".saveBtn").click(function() {
+$(".saveBtn").click(function(event) {
     localStorage.setItem("task", $(this).prev().val());
 
+    // The user's input will be saved to that text area for later use
+    var saveText = $(this).prev();
+    saveText.text(localStorage.getItem("task"));
 })
+
+
 
 
 
